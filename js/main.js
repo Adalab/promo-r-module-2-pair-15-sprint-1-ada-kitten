2122d9ec6fea58f5fd5b323cec926a36bbdc50e4
Marcela B Petroli <marcelabpetroli@gmail.com>
2022-10-18 19:10:21 +0200

    test

--- js/main.js
+++ js/main.js
@@ -3,42 +3,51 @@
 const newForm = document.querySelector(".js-new-form");
 newForm.classList.remove("collapsed");
 
-const infoCats = document.queryselector(".js-list");
-const kittenOne = document.querySelector(".list");
-const kittenTwo = document.querySelector(".list");
-const kittenThree = document.querySelector(".list");
+let newKitten = document.querySelector(".js-list");
 
-kittenOne.innerHTML = `<ul class="list">
-<li class="card">
-  <article>
-    <img class="card_img" src="https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg" alt="gatito" />
-    <h3 class="card_title">Anastacio</h3>
-    <h4 class="card_race">British Shorthair</h4>
-    <p class="card_description">
-      Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!
-    </p>
-  </article>
-</li>
-</ul>`;
-
-kittenTwo.innerHTML = `<ul class="list">
-<li class="card">
-  <img class="card_img" src="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg" alt="gatito" />
-  <h3 class="card_title">Fiona</h3>
-  <h4 class="card_race">British Shorthair</h4>
-  <p class="card_description">Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!</p>
-</li>
-</ul>`;
-
-kittenThree.innerHTML = `<ul class="list">
-<li class="card">
+const kittenOne = `<li class="card">
+<article>
   <img
     class="card_img"
-    src="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg"
+    src="https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg"
     alt="gatito"
   />
-  <h3 class="card_title">Cielo</h3>
+  <h3 class="card_title">Anastacio</h3>
   <h4 class="card_race">British Shorthair</h4>
-  <p class="card_description">Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!</p>
-</li>
-</ul>`;
+  <p class="card_description">
+    Ruiseño, juguetón, le guta estar tranquilo y que nadie le
+    moleste. Es una maravilla acariciarle!
+  </p>
+</article>
+</li>`;
+
+const kittenTwo = ` <li class="card">
+<img
+  class="card_img"
+  src="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg"
+  alt="gatito"
+/>
+<h3 class="card_title">Fiona</h3>
+<h4 class="card_race">British Shorthair</h4>
+<p class="card_description">
+  Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
+  Es una maravilla acariciarle!
+</p>
+</li>`;
+
+const kittenThree = `<li class="card">
+<img
+  class="card_img"
+  src="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg"
+  alt="gatito"
+/>
+<h3 class="card_title">Cielo</h3>
+<h4 class="card_race">British Shorthair</h4>
+<p class="card_description">
+  Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
+  Es una maravilla acariciarle!
+</p>
+</li>`;
+
+const content = kittenOne + kittenTwo + kittenThree;
+newKitten.innerHTML = content;
