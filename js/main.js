1a6792fc7580d38d74313e7e9dddcbadbe8729a6
Becky <rebecachambers919@gmail.com>
2022-10-19 11:13:50 +0200

    push class 2

--- js/main.js
+++ js/main.js
@@ -5,7 +5,65 @@ newForm.classList.remove("collapsed");
 
 let newKitten = document.querySelector(".js-list");
 
+const kittenOneImage = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
+const kittenOneName = "Anastacio";
+const kittenOneDesc = "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+const kittenOneBreed = "British Shorthair";
+
+const kittenTwoImage = "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg";
+const kittenTwoName = "Fiona";
+const kittenTwoDesc = "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+const kittenTwoBreed = "British Shorthair";
+
+const kittenThreeImage = "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg";
+const kittenThreeName = "cielo".toUpperCase();
+
+const kittenThreeDesc = "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+const kittenThreeBreed = "British Shorthair";
+
 const kittenOne = `<li class="card">
+<article>
+  <img class="card_img" src= ${kittenOneImage} alt="gatito" />
+  <h3 class="card_title">${kittenOneName}</h3>
+  <h4 class="card_race">${kittenOneBreed}</h4>
+  <p class="card_description">
+    ${kittenOneDesc}
+  </p>
+</article>
+</li>`;
+
+const kittenTwo = `<li class="card">
+<article>
+  <img class="card_img" src= ${kittenTwoImage} alt="gatito" />
+  <h3 class="card_title">${kittenTwoName}</h3>
+  <h4 class="card_race">${kittenTwoBreed}</h4>
+  <p class="card_description">
+    ${kittenTwoDesc}
+  </p>
+</article>
+</li>`;
+
+const kittenThree = `<li class="card">
+<article>
+  <img class="card_img" src= ${kittenThreeImage} alt="gatito" />
+  <h3 class="card_title">${kittenThreeName}</h3>
+  <h4 class="card_race">${kittenThreeBreed}</h4>
+  <p class="card_description">
+    ${kittenThreeDesc}
+  </p>
+</article>
+</li>`;
+
+const content = kittenOne + kittenTwo + kittenThree;
+newKitten.innerHTML = content;
+
+
+
+
+
+
+
+/*const kittenOne = `<li class="card">
 <article>
   <img
     class="card_img"
@@ -50,4 +108,4 @@ const kittenThree = `<li class="card">
 </li>`;
 
 const content = kittenOne + kittenTwo + kittenThree;
-newKitten.innerHTML = content;
+newKitten.innerHTML = content;*/
