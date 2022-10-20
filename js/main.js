3b882821b4b924403be35eb077b0af7d71d9afa9
Marcela B Petroli <marcelabpetroli@gmail.com>
2022-10-20 19:59:38 +0200

    Add condition to only show cats when the value is a match

--- js/main.js
+++ js/main.js
@@ -7,19 +7,19 @@ let newKitten = document.querySelector(".js-list");
 
 const kittenOneImage = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
 const kittenOneName = "Anastacio";
-const kittenOneDesc = "Ruiseño, juguetón, cariñoso, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+const kittenOneDesc = "Ruiseño, juguetón, cariñoso, le gusta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
 const kittenOneBreed = "British Shorthair";
 
 const kittenTwoImage = "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg";
 const kittenTwoName = "Fiona";
-const kittenTwoDesc = "Ruiseño, juguetón, cariñoso, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+const kittenTwoDesc = "Ruiseño, juguetón, cariñoso, le gusta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
 const kittenTwoBreed = "British Shorthair";
 
 const kittenThreeImage =
   "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg";
 const kittenThreeName = "cielo".toUpperCase();
 
-const kittenThreeDesc = "Ruiseño, juguetón, cariñoso, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+const kittenThreeDesc = "Ruiseño, juguetón, le gusta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
 const kittenThreeBreed = "British Shorthair";
 
 const kittenOne = `<li class="card">
@@ -55,8 +55,8 @@ const kittenThree = `<li class="card">
 </article>
 </li>`;
 
-const content = kittenOne + kittenTwo + kittenThree;
-newKitten.innerHTML = content;
+// const content = kittenOne + kittenTwo + kittenThree;
+// newKitten.innerHTML = content;
 
 /*const kittenOne = `<li class="card">
 <article>
@@ -110,19 +110,19 @@ inputSearchDesc.value = "";
 
 const inputValue = inputSearchDesc.value;
 
-if (kittenOneDesc.includes(inputValue)) {
-  // newKitten.innerHTML = kittenOne;
-  kittenOne.classList.add("hidden");
-}
+//condición para que solo aparezcan gatos si el input tenga valor.
+//si la condición se cumple, analiza cada gato para ver si el valor esta presente.
+//si la condición se cumple se pinta la info del gato
+if (inputValue != "") {
+  if (kittenOneDesc.includes(inputValue)) {
+    newKitten.innerHTML = kittenOne;
+  }
 
-if (kittenTwoDesc.includes(inputValue)) {
-  // newKitten.innerHTML += kittenTwo;
-  kittenTwo.classList.add("hidden");
-}
+  if (kittenTwoDesc.includes(inputValue)) {
+    newKitten.innerHTML += kittenTwo;
+  }
 
-if (kittenThree.includes(inputValue)) {
-  // newKitten.innerHTML += kittenThree;
-  newKitten.classList.add("hidden");
+  if (kittenThreeDesc.includes(inputValue)) {
+    newKitten.innerHTML += kittenThree;
+  }
 }
-
-// intentar añadir lógica de que cuando value este vacio, ningun gato aparezaca
